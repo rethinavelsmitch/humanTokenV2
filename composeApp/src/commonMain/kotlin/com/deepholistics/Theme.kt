193c37dce6e2faa925ee2ca068edaa8018cb8da1
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2025-05-26 19:47:18 +0530

    Init Commit with KMP and CMP

---
+++ composeApp/src/commonMain/kotlin/com/deepholistics/Theme.kt
@@ -0,0 +1,20 @@
+
+package com.deepholistics
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF6C63FF) // Corresponds to #6C63FF
+val PrimaryDark = Color(0xFF5A52E3)
+val PrimaryLight = Color(0xFF8B84FF)
+
+val BackgroundDark = Color(0xFF1A1A1A)
+val SurfaceDark = Color(0xFF2D2D2D)
+val CardBackground = Color(0xFF3A3A3A)
+
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFB0B0B0)
+val TextOnPrimary = Color(0xFFFFFFFF)
+
+val White = Color.White
+val Black = Color.Black
+val Transparent = Color.Transparent
