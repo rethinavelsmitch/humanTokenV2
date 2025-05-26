9b7fa9a903abf3129893799d737307578fa3ca76
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2025-05-26 18:28:40 +0530

    Init Commit with KMP and CMP

--- shared/src/commonMain/kotlin/com/deepholistics/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.deepholistics
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello PuviArasu, ${platform.name}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/deepholistics/Platform.kt
+++
@@ -1,8 +0,0 @@
-package com.deepholistics
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
-
