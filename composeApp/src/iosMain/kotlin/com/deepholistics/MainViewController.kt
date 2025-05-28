0e1ab88f6c04ac5e2961bff4f134435befa6c532
rethinavelsmitch <rethinavel.natarajan@mysmitch.com>
2025-05-28 12:38:41 +0530

    Remove build folders from repo and update .gitignore

--- composeApp/src/iosMain/kotlin/com/deepholistics/MainViewController.kt
+++
@@ -1,6 +0,0 @@
-package com.deepholistics
-
-import androidx.compose.ui.window.ComposeUIViewController
-import com.deepholistics.onboard.App
-
-fun MainViewController() = ComposeUIViewController { App() }
