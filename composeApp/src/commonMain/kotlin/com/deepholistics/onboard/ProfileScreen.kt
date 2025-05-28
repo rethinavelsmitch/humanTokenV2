c5d58efb997b8f4cbeb96351e7d07087f0326e26
rethinavelsmitch <rethinavel.natarajan@mysmitch.com>
2025-05-28 17:20:57 +0530

    Profile screen minor ui changes

--- composeApp/src/commonMain/kotlin/com/deepholistics/onboard/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/deepholistics/onboard/ProfileScreen.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -42,11 +39,11 @@ fun ProfileScreen(onNavigateBack: () -> Unit) {
                     },
                     navigationIcon = {
                         IconButton(onClick = onNavigateBack) {
-                            Icon(
-                                imageVector = Icons.Default.ArrowBack,
-                                contentDescription = "Back",
-                                tint = AppColors.TextPrimary
-                            )
+//                            Icon(
+//                            imageVector = Icons.Default.ArrowBack,
+//                                contentDescription = "Back",
+//                                tint = AppColors.TextPrimary
+//                            )
                         }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
