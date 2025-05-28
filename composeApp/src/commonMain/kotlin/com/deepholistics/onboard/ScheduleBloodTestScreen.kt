d79014dbcc8b48f54f254b9d89f624da02c8ab9b
Puviarasu Maruthachalam <puviarasu.m@mysmitch.com>
2025-05-28 11:14:01 +0530

    add gitignore

--- composeApp/src/commonMain/kotlin/com/deepholistics/onboard/ScheduleBloodTestScreen.kt
+++ composeApp/src/commonMain/kotlin/com/deepholistics/onboard/ScheduleBloodTestScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-importandroidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
